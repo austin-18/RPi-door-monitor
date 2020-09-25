3b3f0cae046383659769b5acc4ecbcd28c8f7193
Austin Bracey <austin.bracey18@gmail.com>
2020-09-25 15:12:00 -0500

    fixed import typo

--- controllers/door.js
+++ controllers/door.js
@@ -2,17 +2,17 @@
 //       All controller files are to begin with a lowercase letter
 
 // Importing ErrorResponse Class (contains a constructor)
-const ErrorResponse = require('../utils/errorResponse');
-// importing the asyncHandler middleware that will eliminate the need for try/catch blocks with next() methods
-const asyncHandler = require('../middleware/async');
+// const ErrorResponse = require('../utils/errorResponse');
+// // importing the asyncHandler middleware that will eliminate the need for try/catch blocks with next() methods
+// const asyncHandler = require('../middleware/async');
 // importing the model into the controller
 const Door = require('../models/Door');
 const path = require('path');
 
 // Controller files conain the Methods for each route - creates functionality for the route
 
-exports.logDoorOpen = asyncHandler(async () => {
+exports.logDoorOpen = async () => {
     const doorOpen = await Door.create()
 
     return doorOpen
-});
+}
