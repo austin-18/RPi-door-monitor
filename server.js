23ce99eb177273c69e4d58f38f50da60d8e157d0
Austin Bracey <austin.bracey18@gmail.com>
2020-09-28 13:35:21 -0500

    testing hig/low pin with LED

--- server.js
+++ server.js
@@ -23,22 +23,25 @@ connectDB();
 
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var highPin = new Gpio(16, 'high'); //use GPIO pin 16 as high output for input into GPIO pin 16
-var doorSwitch = new Gpio(15, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
-doorSwitch.watch(function (err, value) { //Watch for hardware interrupts on doorSwitch GPIO, specify callback function
-  console.log('watching....')
-  if (err) { //if an error
-    console.error('There was an error', err); //output error message to console
-  return;
-  }
-  if(value===0){
-    logDoorOpen();
-    console.log('Door was opened...')
-  } //turn LED on or off depending on the button state (0 or 1)
-});
+var lowPin = new Gpio(15, 'low'); //use GPIO pin 16 as high output for input into GPIO pin 16
+
+// var doorSwitch = new Gpio(15, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
+// doorSwitch.watch(function (err, value) { //Watch for hardware interrupts on doorSwitch GPIO, specify callback function
+//   console.log('watching....')
+//   if (err) { //if an error
+//     console.error('There was an error', err); //output error message to console
+//   return;
+//   }
+//   if(value===0){
+//     logDoorOpen();
+//     console.log('Door was opened...')
+//   } //turn LED on or off depending on the button state (0 or 1)
+// });
 
 function unexportOnClose() { //function to run when exiting program
-  doorSwitch.unexport(); // Unexport Button GPIO to free resources
+  // doorSwitch.unexport(); // Unexport Button GPIO to free resources
   highPin.unexport(); // Unexport Button GPIO to free resources
+  lowPin.unexport();
 };
 
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
