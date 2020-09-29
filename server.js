d4624f3a40a7f1c0c7ead9dded060df6c946789a
Austin Bracey <austin.bracey18@gmail.com>
2020-09-29 17:21:32 -0500

    added a few console logs for testing

--- server.js
+++ server.js
@@ -20,21 +20,25 @@ dotenv.config({ path: './config/config.env'});
 
 // while(1) { monitor GPIO pin for change => call logDoorOpen to create new datapoint on server }
 
-
+console.log('Initializing GPIO....')
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var highPin = new Gpio(4, 'high'); //use GPIO pin 16 as high output for input into GPIO pin 16
-
 var doorSwitch = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
+
+console.log('GPIO Initialized.')
+console.log('Watching Door...')
+
 doorSwitch.watch(function (err, value) { //Watch for hardware interrupts on doorSwitch GPIO, specify callback function
-  console.log('watching....')
   if (err) { //if an error
     console.error('There was an error', err); //output error message to console
   return;
   }
   if(value===0){
-    logDoorOpen();
+    //logDoorOpen();
     console.log('Door was opened...')
   } //turn LED on or off depending on the button state (0 or 1)
+  else if(value===1){
+    console.log('Door was closed...')
+  }
 });
 
 function unexportOnClose() { //function to run when exiting program
